358017390c630bf878ae6bd908349125d0d5a680
eric-wojcik1999 <46893092+eric-wojcik1999@users.noreply.github.com>
2026-03-22 20:45:43 +0800

    Refining the turret

--- Entities/Enemies/TurretEnemyBasic.cs
+++ Entities/Enemies/TurretEnemyBasic.cs
@@ -7,7 +7,7 @@ public partial class TurretEnemyBasic : CharacterBody3D
 	[ExportGroup("Node Paths")]
     [Export] private NodePath _yawPivotPath = "YawPivot"; // Yaw rotates here
     [Export] private NodePath _pivotPitchPath = "YawPivot/PitchPivot"; // Pitch rotates here
-    [Export] private NodePath _muzzlePath = "yawPivot/PitchPivot/MuzzleMarker";
+    [Export] private NodePath _muzzleMarkerPath = "YawPivot/PitchPivot/MuzzleMarker";
     [Export] private NodePath _losPath = "YawPivot/PitchPivot/MuzzleMarker/LineOfSightRayCast";
     [Export] private NodePath _aggroRangePath = "AggroRange";
 
@@ -15,13 +15,12 @@ public partial class TurretEnemyBasic : CharacterBody3D
     [Export] private float _turnSpeedYaw = 6.0f;
     [Export] private float _turnSpeedPitch = 6.0f;
     [Export] private bool _usePitch = true;
-    [Export] private float _minPitchDeg = -20f;
-    [Export] private float _maxPitchDeg = 20f;
+    [Export] private float _minPitchDeg = -40f;
+    [Export] private float _maxPitchDeg = 40f;
 
     [ExportGroup("Firing")]
-    [Export] private PackedScene _plasmaBallScene;
-    [Export] private float _fireInterval = 1.0f;
-    [Export] private float _muzzleForwardSpawnOffset = 0.2f; // Probably not needed
+    [Export] public PackedScene _plasmaBallScene;
+    [Export] private float _rateOfFire = 1.0f;
     [Export] private float _maxFireDistance = 999f;
 
     // Don't neeeed to type as MeshInstance3D here (even though they are nearly all MeshInstance3D nodes)
@@ -29,12 +28,12 @@ public partial class TurretEnemyBasic : CharacterBody3D
     // If need to change mesh specific things like mesh colour, then yes type it as a MeshInstance3D
     private Node3D _yawPivot;
     private Node3D _pitchPivot;
-    private Node3D _muzzle;
+    private Node3D _muzzleMarker;
     private RayCast3D _losRay;
     private Area3D _aggroRange;
 
-    // This is actually overkill since there is only ever one target, but keeping anyway
-    private readonly HashSet<Node3D> _possibleTargets = new HashSet<Node3D>(); 
+    // // This is actually overkill since there is only ever one target, but keeping anyway
+    // private readonly HashSet<Node3D> _possibleTargets = new HashSet<Node3D>(); 
     private Node3D _target;
     private float _fireTimer;
 
@@ -42,13 +41,13 @@ public partial class TurretEnemyBasic : CharacterBody3D
     {
         _yawPivot = GetNodeOrNull<Node3D>(_yawPivotPath);
         _pitchPivot = GetNodeOrNull<Node3D>(_pivotPitchPath);
-        _muzzle = GetNodeOrNull<Node3D>(_muzzlePath);
+        _muzzleMarker = GetNodeOrNull<Node3D>(_muzzleMarkerPath);
         _losRay = GetNodeOrNull<RayCast3D>(_losPath);
         _aggroRange = GetNodeOrNull<Area3D>(_aggroRangePath);
 
         if (_yawPivot == null) GD.PushError("[TurretEnemyBasic] Core path is invalid");
         if (_pitchPivot == null) GD.PushError("[TurretEnemyBasic] Barrel path is invalid");
-        if (_muzzle == null) GD.PushError("[TurretEnemyBasic] Muzzle path is invalid");
+        if (_muzzleMarker == null) GD.PushError("[TurretEnemyBasic] Muzzle path is invalid");
         if (_losRay == null) GD.PushError("[TurretEnemyBasic] LineOfSight path is invalid");
         if (_aggroRange == null) GD.PushError("[TurretEnemyBasic] Core path is invalid");
 
@@ -58,7 +57,7 @@ public partial class TurretEnemyBasic : CharacterBody3D
             _aggroRange.BodyExited += OnAggroBodyExited;
         }
 
-        _fireTimer = _fireInterval;
+        _fireTimer = _rateOfFire;
     }
 
     public override void _PhysicsProcess(double delta)
@@ -79,13 +78,14 @@ public partial class TurretEnemyBasic : CharacterBody3D
         // CHECK LINE OF SIGHT 
 
         // FIRE IF IN LOS
+        HandleAutoFire(dt);
     }
 
     private void AimAtTarget(Node3D target, float dt)
     {
-        if (_yawPivot == null || _pitchPivot == null || _muzzle == null) return;
+        if (_yawPivot == null || _pitchPivot == null || _muzzleMarker == null) return;
 
-        Vector3 muzzlePos = _muzzle.GlobalPosition;
+        Vector3 muzzlePos = _muzzleMarker.GlobalPosition;
         Vector3 toTarget = (target.GlobalPosition - muzzlePos);
 
         // Is there enough horizontal distance between the turrent and the target to compute yaw
@@ -112,8 +112,6 @@ public partial class TurretEnemyBasic : CharacterBody3D
 
         // PITCH: Rotate barrel around X only (for aiming up and down)
         Vector3 localTarget = _pitchPivot.ToLocal(target.GlobalPosition);
-
-        // float desiredPitch = Mathf.Atan2(localTarget.Y, -localTarget.Z);
         float desiredPitch = Mathf.Atan2(localTarget.Y, localTarget.Z);
         float desiredPitchDeg = Mathf.Clamp(Mathf.RadToDeg(desiredPitch), _minPitchDeg, _maxPitchDeg);
         desiredPitch = Mathf.DegToRad(desiredPitchDeg);
@@ -123,6 +121,43 @@ public partial class TurretEnemyBasic : CharacterBody3D
         float pitchStep = Mathf.Clamp(_turnSpeedPitch * dt, 0.0f, 1.0f);
         pitchRot.X += pitchDiff * pitchStep;
         _pitchPivot.Rotation = pitchRot;
+    }
+
+    private void HandleAutoFire(float dt)
+    {
+        if (_plasmaBallScene == null || _muzzleMarker == null) return;
+
+    	// Decrease timer towards 0
+		_fireTimer -= dt;
+
+		// If timer still has time left, ignore rest of function
+		if (_fireTimer > 0f) return;
+
+		// Reset timer if ROF
+		_fireTimer = _rateOfFire;
+		SpawnPlasmaBall();
+    }
+
+    private void SpawnPlasmaBall() 
+    {
+        if (_plasmaBallScene == null)
+        {
+            GD.PrintErr("Bullet scene not assigned to player!");
+            return;
+        }
+
+		// Find Turret's forward vector to be direction for bullets
+		Vector3 shootDirection = _muzzleMarker.GlobalTransform.Basis.Z;
+		shootDirection = shootDirection.Normalized();
+
+		TurretEnemyPlasmaBall plasmaBall = _plasmaBallScene.Instantiate<TurretEnemyPlasmaBall>();
+		
+		// Add plasma ball to scene (use current scene root)
+    	GetTree().CurrentScene.AddChild(plasmaBall);
+
+		plasmaBall.GlobalPosition = _muzzleMarker.GlobalPosition;
+		plasmaBall.GlobalRotation = _muzzleMarker.GlobalRotation;
+		plasmaBall.Direction = shootDirection;
     }
 
     private void OnAggroBodyEntered(Node3D body)
@@ -141,7 +176,7 @@ public partial class TurretEnemyBasic : CharacterBody3D
     {
         if (body == null) return;
 
-        _possibleTargets.Remove(body);
+        // _possibleTargets.Remove(body);
 
         if (_target == body) 
         {
--- Entities/Enemies/TurretEnemyPlasmaBall.cs
+++ Entities/Enemies/TurretEnemyPlasmaBall.cs
@@ -3,13 +3,50 @@ using System;
 
 public partial class TurretEnemyPlasmaBall : Area3D
 {
-	// Called when the node enters the scene tree for the first time.
+	[Export] private float _speed = 40f;
+	[Export] private float _lifeTime = 3.0f;
+	[Export] private int _turretDamage = 1;
+	[Export] private float _growTime = 0.12f;
+	public Vector3 Direction { get; set; } = Vector3.Forward;
+
+	private float _lifeTimer;
+	private float _growTimer;
+	private float _test = 0.1f;
+
 	public override void _Ready()
 	{
+		_lifeTimer = _lifeTime;
+		_growTimer = 0f;
+		Scale = Vector3.One * 0.1f;
+
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		float dt = (float)delta;
+
+        if (_growTimer < _growTime)
+        {
+            _growTimer += dt;
+            float t = Mathf.Clamp(_growTimer / _growTime, 0.0f, 0.65f);
+            Scale = Vector3.One * Mathf.Max(t, 0.001f);
+        }
+
+
+		GlobalPosition += Direction * _speed * dt;
+
+		_lifeTimer -= dt;
+
+		if (_lifeTimer < 0.0f) QueueFree();
+	}
+
+	private void OnBodyEntered(Node3D body)
+	{
+		if (body is PlayerGround player)
+		{
+			player.TakeDamage(_turretDamage);
+		}
+
+		QueueFree();
 	}
 }
