ba600281f2e380f6a666dddb2171481ef75620c0
eric-wojcik1999 <46893092+eric-wojcik1999@users.noreply.github.com>
2026-03-22 21:06:50 +0800

    Refining the turret #2

--- Entities/Enemies/TurretEnemyBasic.cs
+++ Entities/Enemies/TurretEnemyBasic.cs
@@ -10,6 +10,9 @@ public partial class TurretEnemyBasic : CharacterBody3D
     [Export] private NodePath _muzzleMarkerPath = "YawPivot/PitchPivot/MuzzleMarker";
     [Export] private NodePath _losPath = "YawPivot/PitchPivot/MuzzleMarker/LineOfSightRayCast";
     [Export] private NodePath _aggroRangePath = "AggroRange";
+    [Export] private NodePath _sideDetectionPath = "SideDetection";
+    
+    [Export] private NodePath _topDetectionPath = "TopDetection";
 
     [ExportGroup("Targeting")]
     [Export] private float _turnSpeedYaw = 6.0f;
@@ -23,6 +26,10 @@ public partial class TurretEnemyBasic : CharacterBody3D
     [Export] private float _rateOfFire = 1.0f;
     [Export] private float _maxFireDistance = 999f;
 
+    [ExportGroup("Properties")]
+    [Export] public int _damageAmount = 1;
+	[Export] public int _currencyAmount = 1;
+
     // Don't neeeed to type as MeshInstance3D here (even though they are nearly all MeshInstance3D nodes)
     // because Node3D can be better in this case as it has everything we need for what we want to accomplish
     // If need to change mesh specific things like mesh colour, then yes type it as a MeshInstance3D
@@ -31,6 +38,9 @@ public partial class TurretEnemyBasic : CharacterBody3D
     private Node3D _muzzleMarker;
     private RayCast3D _losRay;
     private Area3D _aggroRange;
+    private Area3D _sideDetection;
+    
+    private Area3D _topDetection;
 
     // // This is actually overkill since there is only ever one target, but keeping anyway
     // private readonly HashSet<Node3D> _possibleTargets = new HashSet<Node3D>(); 
@@ -44,6 +54,10 @@ public partial class TurretEnemyBasic : CharacterBody3D
         _muzzleMarker = GetNodeOrNull<Node3D>(_muzzleMarkerPath);
         _losRay = GetNodeOrNull<RayCast3D>(_losPath);
         _aggroRange = GetNodeOrNull<Area3D>(_aggroRangePath);
+		_sideDetection = GetNode<Area3D>(_sideDetectionPath);
+		_sideDetection.BodyEntered += OnSideDetectionPlayerEntered;
+		_topDetection = GetNode<Area3D>(_topDetectionPath);
+		_topDetection.BodyEntered += OnTopDetectionPlayerEntered;
 
         if (_yawPivot == null) GD.PushError("[TurretEnemyBasic] Core path is invalid");
         if (_pitchPivot == null) GD.PushError("[TurretEnemyBasic] Barrel path is invalid");
@@ -111,6 +125,7 @@ public partial class TurretEnemyBasic : CharacterBody3D
         if (!_usePitch) return;
 
         // PITCH: Rotate barrel around X only (for aiming up and down)
+        // Something is slightly off with the pitch, keeps jittering back upwards. FIX LATER
         Vector3 localTarget = _pitchPivot.ToLocal(target.GlobalPosition);
         float desiredPitch = Mathf.Atan2(localTarget.Y, localTarget.Z);
         float desiredPitchDeg = Mathf.Clamp(Mathf.RadToDeg(desiredPitch), _minPitchDeg, _maxPitchDeg);
@@ -183,4 +198,44 @@ public partial class TurretEnemyBasic : CharacterBody3D
             _target = null;
         }
     }
+
+	private void OnSideDetectionPlayerEntered(Node3D body) 
+	{
+		if (body != null)
+		{
+			// Disable top collision when side ccollision entered to prevent double collision
+			_topDetection.SetCollisionMaskValue(1, false);
+
+			if (body is PlayerGround player)
+			{
+				player.TakeDamage(_damageAmount);
+				player.BounceBackFromPosition(GlobalPosition, 4f);
+				QueueFree();
+			}
+		}
+		else 
+		{
+			GD.PushError("[TurretEnemyBasic] player body is null.");
+		}
+	}
+
+	private void OnTopDetectionPlayerEntered(Node3D body) 
+	{
+		if (body != null)
+		{
+			// Disable side collision when top ccollision entered to prevent double collision
+			_sideDetection.SetCollisionMaskValue(1, false);
+
+			if (body is PlayerGround player)
+			{
+				player.BounceUp();
+				Global.Instance.AddCurrency(_currencyAmount);
+				QueueFree();
+			}
+		}
+		else 
+		{
+			GD.PushError("[TurretEnemyBasic] player body is null.");
+		}
+	}
 }
